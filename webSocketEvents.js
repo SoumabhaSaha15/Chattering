53dc17b8ca0cfe954015a20a2ce20ce69e0648cd
SoumabhaSaha15 <soumabhasaha1509+github@gmail.com>
2024-03-31 23:59:09 +0530

    socket events

--- webSocketEvents.js
+++ webSocketEvents.js
@@ -1,14 +1,48 @@
+import { Server, Socket } from "socket.io";
 const EVENTS = {
   ON:{
     UserData:{
       name:'UserData',
-      execute:(data)=>{
-        
+      /**
+       * @name UserData event  
+       * @param {Server} io 
+       * @param {Socket} socket 
+       * @param {Object} data 
+       * @param {Object} all_user
+       */
+      execute:(io,socket,data,all_user)=>{
+        console.log(data);
+        if(data['err_msg']){
+          socket.disconnect(true);
+        }else{
+          all_user[socket.id] = {
+            UserName:data['UserName'],
+            Email:data['Email'],
+            DP:data['DP']
+          }
+          io.emit(EVENTS.EMIT.AllUser.name,all_user);
+        }
+      }
+    },
+    disconnect:{
+      name:'disconnect',
+      /**
+       * @name disconnect event
+       * @param {Server} io 
+       * @param {Socket} socket 
+       * @param {import("socket.io").DisconnectReason} data
+       * @param {Object} 
+       */
+      execute:(io,socket,data,userData)=>{
+        delete userData[socket.id];
+        io.emit(EVENTS.EMIT.AllUser.name,userData);
       }
     }
   },
   EMIT:{
-    Wellcome:'Wellcome',
+    AllUser:{
+      name:'AllUser'
+    },
   }
 }
 export default EVENTS;
